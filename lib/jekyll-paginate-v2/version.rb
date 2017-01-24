6cfdf428907fe3836efacc8e5f299dc6eb808bd7
Sverrir Sigmundarson <sverrirs@gmail.com>
2017-01-24 19:08:27 +0100

    Bump version

--- lib/jekyll-paginate-v2/version.rb
+++ lib/jekyll-paginate-v2/version.rb
@@ -1,8 +1,8 @@
 module Jekyll
   module PaginateV2
-    VERSION = "1.6.0"
+    VERSION = "1.6.1"
     # When modifying remember to issue a new tag command in git before committing, then push the new tag
-    # git tag -a v1.6.0 -m "Gem v1.6.0"
+    # git tag -a v1.6.1 -m "Gem v1.6.1"
     # git push origin --tags
   end # module PaginateV2
 end # module Jekyll
