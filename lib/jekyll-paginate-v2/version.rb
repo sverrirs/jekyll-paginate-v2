a18d2d4c24d7afa8ea40634f8c1bc31aa329ee84
Sverrir Sigmundarson <sverrirs@gmail.com>
2018-01-24 17:23:49 +0100

    Bumping version to v1.9.4 for release

--- lib/jekyll-paginate-v2/version.rb
+++ lib/jekyll-paginate-v2/version.rb
@@ -1,8 +1,8 @@
 module Jekyll
   module PaginateV2
-    VERSION = "1.9.2"
+    VERSION = "1.9.4"
     # When modifying remember to issue a new tag command in git before committing, then push the new tag
-    #   git tag -a v1.9.2 -m "Gem v1.9.2"
+    #   git tag -a v1.9.4 -m "Gem v1.9.4"
     #   git push origin --tags
     # Yanking a published Gem
     #   gem yank jekyll-paginate-v2 -v VERSION
