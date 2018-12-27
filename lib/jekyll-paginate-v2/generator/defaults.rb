70e7e50be5f4edee6906de5d39057f37958b9204
Ashwin Maroli <ashmaroli@gmail.com>
2018-12-27 20:41:28 +0530

    Set 'index' as the default basename of indexpage

--- lib/jekyll-paginate-v2/generator/defaults.rb
+++ lib/jekyll-paginate-v2/generator/defaults.rb
@@ -17,7 +17,7 @@ module Jekyll
           'before' => 0, # Limits how many links to show before the current page in the pagination trail (0, means off, default: 0)
           'after' => 0,  # Limits how many links to show after the current page in the pagination trail (0 means off, default: 0)
       },
-      'indexpage'    => nil, # The default name of the index pages
+      'indexpage'    => 'index', # The default name of the index pages
       'extension'    => 'html', # The default extension for the output pages (ignored if indexpage is nil)
       'debug'        => false, # Turns on debug output for the gem
       'legacy'       => false # Internal value, do not use (will be removed after 2018-01-01)
