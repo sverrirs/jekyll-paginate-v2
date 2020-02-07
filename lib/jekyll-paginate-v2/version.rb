53ea42fee99862fa24d47fdef0834841e7c5ac10
Sverrir Sigmundarson <sverrirs@gmail.com>
2020-02-07 23:55:47 +0000

    Rollback to last stable 1.9.4 with selective cherry picking

--- lib/jekyll-paginate-v2/version.rb
+++ lib/jekyll-paginate-v2/version.rb
@@ -1,10 +1,12 @@
 module Jekyll
   module PaginateV2
-    VERSION = "2.0.0"
+    VERSION = "3.0.0"
     # When modifying remember to issue a new tag command in git before committing, then push the new tag
-    #   git tag -a v2.0.0 -m "Gem v2.0.0"
+    #   git tag -a v3.0.0 -m "Gem v3.0.0"
     #   git push origin --tags
     # Yanking a published Gem
     #   gem yank jekyll-paginate-v2 -v VERSION
+    # Building gem
+    #   gem build jekyll-paginate-v2.gemspec
   end # module PaginateV2
 end # module Jekyll
