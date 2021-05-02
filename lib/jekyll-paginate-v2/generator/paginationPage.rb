559ac093cf6032e7c2d23f801d2b921611f8d7b7
Liam Bigelow <40188355+bglw@users.noreply.github.com>
2021-05-03 10:06:44 +1200

    Retain page extension after pagination (#215)
    Co-authored-by: Sverrir Sigmundarson <sverrirs@gmail.com>

--- lib/jekyll-paginate-v2/generator/paginationPage.rb
+++ lib/jekyll-paginate-v2/generator/paginationPage.rb
@@ -32,6 +32,9 @@ module Jekyll
         # Store the current page and total page numbers in the pagination_info construct
         self.data['pagination_info'] = {"curr_page" => cur_page_nr, 'total_pages' => total_pages }       
 
+        # Retain the extention so the page exists in site.html_pages
+        self.ext = page_to_copy.ext
+        
         # Map the first page back to the source file path, to play nice with other plugins
         self.data['path'] = page_to_copy.path if cur_page_nr == 1
 
