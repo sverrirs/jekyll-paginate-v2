573d0da9e6f1142c60115a713e876bdffdce2930
Cherryleafroad <13651622+cherryleafroad@users.noreply.github.com>
2022-03-31 16:42:14 -0700

    Rework autopaging implementation + allow custom filters for collections

--- lib/jekyll-paginate-v2/autopages/autoPages.rb
+++ lib/jekyll-paginate-v2/autopages/autoPages.rb
@@ -5,10 +5,16 @@ module Jekyll
     # This function is called right after the main generator is triggered by Jekyll
     # This code is adapted from Stephen Crosby's code https://github.com/stevecrozz
     def self.create_autopages(site)
-
       # Get the configuration for the auto pages
-      autopage_config = Jekyll::Utils.deep_merge_hashes(DEFAULT, site.config['autopages'] || {})
+      autopage_config = site.config['autopages'] || {}
       pagination_config = Jekyll::Utils.deep_merge_hashes(Jekyll::PaginateV2::Generator::DEFAULT, site.config['pagination'] || {})
+
+      # Simply gather all documents across all pages/posts/collections that we have
+      # and store them under each collection name
+      coll_docs = Utils.collect_all_docs(site.collections)
+      # we don't use the posts collection
+      coll_docs.delete("posts")
+      coll_names = coll_docs.keys
 
       # If disabled then don't do anything
       if !autopage_config['enabled'] || autopage_config['enabled'].nil?
@@ -16,56 +22,26 @@ module Jekyll
         return
       end
 
-      autopages_log(autopage_config, 'tags', 'categories', 'collections')
+      autopages_log(autopage_config, *coll_names)
 
-      # TODO: Should I detect here and disable if we're running the legacy paginate code???!
+      coll_docs.each do |coll_name, coll_data|
+        createcolpage_lambda = lambda do |autopage_col_config, doc|
+          site.pages << AutoPage.new(site, autopage_col_config, pagination_config, coll_name, doc)
+        end
 
-      # Simply gather all documents across all pages/posts/collections that we have
-      # we could be generating quite a few empty pages but the logic is just vastly simpler than trying to 
-      # figure out what tag/category belong to which collection.
-      posts_to_use = Utils.collect_all_docs(site.collections)
-
-      ###############################################
-      # Generate the Tag pages if enabled
-      createtagpage_lambda = lambda do | autopage_tag_config, pagination_config, layout_name, tag, tag_original_name |
-        site.pages << TagAutoPage.new(site, site.dest, autopage_tag_config, pagination_config, layout_name, tag, tag_original_name)
+        autopage_create(autopage_config, coll_name, coll_data, createcolpage_lambda)
       end
-      autopage_create(autopage_config, pagination_config, posts_to_use, 'tags', 'tags', createtagpage_lambda) # Call the actual function
-      
-
-      ###############################################
-      # Generate the category pages if enabled
-      createcatpage_lambda = lambda do | autopage_cat_config, pagination_config, layout_name, category, category_original_name |
-        site.pages << CategoryAutoPage.new(site, site.dest, autopage_cat_config, pagination_config, layout_name, category, category_original_name)
-      end
-      autopage_create(autopage_config, pagination_config,posts_to_use, 'categories', 'categories', createcatpage_lambda) # Call the actual function
-      
-      ###############################################
-      # Generate the Collection pages if enabled
-      createcolpage_lambda = lambda do | autopage_col_config, pagination_config, layout_name, coll_name, coll_original_name |
-        site.pages << CollectionAutoPage.new(site, site.dest, autopage_col_config, pagination_config, layout_name, coll_name, coll_original_name)
-      end
-      autopage_create(autopage_config, pagination_config,posts_to_use, 'collections', '__coll', createcolpage_lambda) # Call the actual function
-    
     end # create_autopages
-
 
     # STATIC: this function actually performs the steps to generate the autopages. It uses a lambda function to delegate the creation of the individual
     #         page types to the calling code (this way all features can reuse the logic).
     #
-    def self.autopage_create(autopage_config, pagination_config, posts_to_use, configkey_name, indexkey_name, createpage_lambda )
-      if !autopage_config[configkey_name].nil?
+    def self.autopage_create(autopage_config, configkey_name, coll_data, createpage_lambda)
+      unless autopage_config[configkey_name].nil?
         ap_sub_config = autopage_config[configkey_name]
-        if ap_sub_config ['enabled']
-          # Roll through all documents in the posts collection and extract the tags
-          index_keys = Utils.ap_index_posts_by(posts_to_use, indexkey_name) # Cannot use just the posts here, must use all things.. posts, collections...
-
-          index_keys.each do |index_key, value|
-            # Iterate over each layout specified in the config
-            ap_sub_config ['layouts'].each do | layout_name |
-              # Use site.dest here as these pages are never created in the actual source but only inside the _site folder
-              createpage_lambda.call(ap_sub_config, pagination_config, layout_name, index_key, value[-1]) # the last item in the value array will be the display name
-            end
+        if ap_sub_config['enabled']
+          coll_data.each do |doc|
+              createpage_lambda.call(ap_sub_config, doc)
           end
         end
       end
